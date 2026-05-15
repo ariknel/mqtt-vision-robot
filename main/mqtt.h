6d1b315e62426a2fcbf294276a548ccd3c62cb3b
Arik <74555054+ariknel@users.noreply.github.com>
2026-05-15 16:54:17 +0200

    Delete main directory

--- main/mqtt.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdbool.h>
-#include "sensors.h"
-
-void  mqtt_init(void);
-bool  mqtt_connected(void);
-void  mqtt_publish_telemetry(IRData ir, UltraData ultra, float battery);
--- main/sensors.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct { int left, center, right; } IRData;
-typedef struct { int left, center, right; } UltraData;
-
-void      sensors_init(void);
-IRData    sensors_read_ir(void);
-UltraData sensors_read_ultrasonic(void);
-float     sensors_read_battery(void);
